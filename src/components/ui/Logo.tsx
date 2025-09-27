ea9e0e2344a40ed532ec4a64aec99c26c16e0585
Fathima <219388167+fathimaat@users.noreply.github.com>
2025-09-27 19:06:04 +0530

    Updated index.html

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export function Logo() {
   return (
-    <a href="https://razinrayees.com" className="font-['Be_Vietnam'] font-semibold text-[30px] leading-[100%]">
-      RAZIN RAYEES
+    <a href="#" className="font-['Be_Vietnam'] font-semibold text-[30px] leading-[100%]">
+      rr.
     </a>
   );
 }
